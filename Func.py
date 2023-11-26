58c36a01c34676917606be8089cfc0529e0ede14
wertkop <pfffff1901@gmail.com>
2023-11-26 22:03:30 +0300

    2stcomm

--- Func.py
+++ Func.py
@@ -1,20 +1,26 @@
 #-- reading FASTA --
-def read_fasta(file_path):
-    with open(file_path, 'r') as file:
+def read_fasta():
+    with open('fasta.fsa', 'r') as file:
         sequences = {}
         sequence_id = None
         sequence = ''
         seq_counter = 1
+
         for line in file:
+
             line = line.strip()
             if line.startswith('>'):
                 if sequence_id is not None:
                     sequences[sequence_id] = sequence
                 sequence_id = 'Seq' + str(seq_counter)
-                seq_counter +=1
+                seq_counter += 1
                 sequence = ''
             else:
                 sequence += line
         if sequence_id is not None:
             sequences[sequence_id] = sequence
-        return sequences
+        dna_sequence = []
+        for key, value in sequences.items():
+            dna_sequence.append(value)
+
+        return dna_sequence
